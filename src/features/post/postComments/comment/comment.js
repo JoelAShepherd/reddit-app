34e19bcae2f1b2fb2f71034a892213e0abac2137
JoelAShepherd <joel125shepherd@gmail.com>
2021-07-13 16:50:40 +0100

    css improvements for comments

--- src/features/post/postComments/comment/comment.js
+++ src/features/post/postComments/comment/comment.js
@@ -1,20 +1,37 @@
 import styles from './comment.module.css'
+import redditLogo from '../../../../images/redditLogo.png';
 
 
 export const Comment = (props) => {
     const replies = props.data.replies
     console.log(replies)
 
+    if (!props.body) {
+        return (
+            <div></div>
+        )
+    }
+
+    const barStyle = {
+        "width": "5px !important"
+    }
+
     return (
-        <div className={styles.commentContainer} style={{paddingLeft: props.depth * 8}}>
-            {props.body && <div className={styles.bqBar}></div>}
-            
-            <div className={styles.innerContainer}>
-            <p>{props.body}</p>
-            {replies && replies.data.children.map(reply => 
-                <Comment body={reply.data.body} data={reply.data} key={reply.data.id} depth={props.depth + 1}/>
-                )}
+        <div className={styles.commentContainer} >
+            <div className={styles.bqBar} ></div>
+            <div>
+                <div className={styles.authorContainer}>
+                    <img className={styles.commentAuthorImg} src={redditLogo} alt=""/>
+                    <p className={styles.authorName}>{props.data.author}</p>
+                </div>
+                <div className={styles.innerContainer}>
+                    <p className={styles.commentBody}>{props.body}</p>
+                    {replies && replies.data.children.map(reply => 
+                        <Comment body={reply.data.body} data={reply.data} key={reply.data.id} depth={props.depth + 1}/>
+                        )}
+                </div>
             </div>
         </div>
     )
 }
+
