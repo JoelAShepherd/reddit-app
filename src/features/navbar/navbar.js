643e419d80553f5f52b5718ae51ce7e68558e216
JoelAShepherd <joel125shepherd@gmail.com>
2021-07-16 15:36:25 +0100

    updates to the navbar

--- src/features/navbar/navbar.js
+++ src/features/navbar/navbar.js
@@ -7,8 +7,13 @@ import styles from './navbar.module.css'
 export const Navbar = () => {
 
     return (
-        <Link to="/">
-            <img src={redditLogo} className={styles.logo} alt="" />
-        </Link>
+        
+            <div className={styles.navContainer}>
+                <Link to="/" className={styles.navLink}>
+                    <h1 className={styles.title}>Reddit<span className={styles.mini}>mini</span></h1>
+                    <img src={redditLogo} className={styles.logo} alt="" />
+                </Link>
+            </div>
+        
     )
 }
